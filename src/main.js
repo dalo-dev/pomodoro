3e80f615c070a5dd60c02713f69968b77b728aff
DALO <dalodev@proton.me>
2024-07-01 19:35:35 -0500

    feat(timer): add timer countdown

--- src/main.js
+++ src/main.js
@@ -1,1 +1,25 @@
 import "./styles/main.css";
+
+const timerElm = document.getElementById("timer");
+
+let intervalId;
+let count = 900;
+
+const setTimer = function (count) {
+  let minutes = Math.trunc(count / 60);
+  let seconds = count % 60;
+  timerElm.textContent = `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+};
+
+const countdown = function () {
+  count--;
+  setTimer(count);
+};
+
+const startCountdown = function () {
+  if (!intervalId) {
+    intervalId = setInterval(countdown, 1000);
+  }
+};
+setTimer(count);
+startCountdown();
